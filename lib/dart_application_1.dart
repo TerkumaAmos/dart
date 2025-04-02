7673239d37e14270b3c78188bd426d324fb3909d
TerkumaAmos <agberterkuma2020@gmail.com>
2025-04-02 12:06:42 +0100

    new

--- lib/dart_application_1.dart
+++ lib/dart_application_1.dart
@@ -1,16 +1,30 @@
+// void main() {
+//   print('${Cookie().size} cm ');
+// }
+
+// class Cookie {
+//   String shape = 'Circle';
+//   double size = 15.2;
+
+//   void baking() {
+//     print('Baking has Started');
+//   }
+
+//   bool isCooling() {
+//     return false;
+//   }
+// }
 void main() {
-  print('${Cookie().size} cm ');
-}
-
-class Cookie {
-  String shape = 'Circle';
-  double size = 15.2;
-
-  void baking() {
-    print('Baking has Started');
+  const trafficLight = 'yellow';
+  var command = '';
+  if (trafficLight == 'red') {
+    command = 'stop';
+  } else if (trafficLight == 'yellow') {
+    command = 'slow down';
+  } else if (trafficLight == 'green') {
+    command = 'Go';
+  } else {
+    command = "invalid color";
   }
-
-  bool isCooling() {
-    return false;
-  }
+  print(command);
 }
