79d82e598efb8eb2efdab8210a9ff8957ab4046c
TerkumaAmos <agberterkuma2020@gmail.com>
2025-04-10 22:44:40 +0100

    new

--- bin/practice.dart
+++ bin/practice.dart
@@ -1,25 +1,4 @@
-void main (){
-  num a = 3;
-  num b = 5;
-
-  //print(a+b);
-  //print(a-b);
-  //print(a/b);
- // print(a*b);
- 
-
- //print(a);
- int age = 18;
- if (age>=18){
-  print("Adult");
-
- }
-  if (age>=21){
-  print("Adult 21  ");
- }
-else{
-  print("Child");
-}
- 
-
+void main() {
+  String nameOfOrganization = "Odyssey Educational Foundation";
+  print(nameOfOrganization);
 }
