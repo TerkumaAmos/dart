227071ba7a10252d6169c2b3acf56e9d87366a27
TerkumaAmos <agberterkuma2020@gmail.com>
2025-04-13 11:34:32 +0100

    new

---
+++ bin/dart_apprentice.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: dead_code
+
+void main() {
+  const doesOneNotEqualTwo = (1 != 2);
+  // print(doesOneNotEqualTwo);
+  const isOneGreaterThanTwo = (1 > 2);
+  //print(isOneGreaterThanTwo);
+  const isSunny = true;
+  const isFinished = true;
+  const willGoCycling = isSunny && isFinished;
+  //print(willGoCycling);
+  const willTravelToAustralia = true;
+  const canFindPhoto = true;
+  const canDrawPlatypus = willTravelToAustralia || canFindPhoto;
+  // print(canDrawPlatypus);
+  const andTrue = 1 < 2 && 4 > 3;
+  //print(andTrue);
+  const andFalse = 1 < 2 && 3 > 4;
+  //print(andFalse);
+  const orTrue = (3 > 4 && 1 < 2) || 1 < 4;
+  print(orTrue);
+  // var myPreciousData = 5782;
+  // String anInnocentLookingFunction(String name){\}
+}
